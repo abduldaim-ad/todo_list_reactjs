8e0d4840960447974d17b2d90585f3d075d25b02
ad551 <gdaim551@gmail.com>
2022-03-06 22:08:05 +0500

    Remove Item Feature Added

--- src/App.js
+++ src/App.js
@@ -17,16 +17,48 @@ const App = () =>{
     }
   }
 
+  const HandleRemove = () =>{
+    if(item!==""){
+      setItemList((itemList)=>{
+        return itemList.filter((arrElem, index)=>{
+          return arrElem!==item;
+        })
+      })
+      return(
+        <>
+          {setItem("")}
+        </>
+      )
+    }
+  }
+
+  const HandleSort = () =>{
+    console.log(itemList.sort());
+    alert(itemList.sort());
+    //const sortedList = itemList.sort();
+    // if(item!==""){
+    //   setItemList(sortedList);
+    // }
+    // return (
+    //   <>
+    //   {sortedList}
+    //   {setItem("")}
+    //   </>
+    // );
+  }
+
   return(
     <>
       <h1>ToDo List</h1>
       <input value={item} onChange={(e)=>setItem(e.target.value)} placeholder="Add an item"/>
       <ul>
-      {itemList. map((item) => {
-      return <li key={item}>{item}</li>;
+      {itemList.map((item) => {
+      return <li id={item} key={item}>{item}</li>;
       })}
       </ul>
       <button onClick={HandleAdd}>Add</button>
+      <button onClick={HandleRemove}>Remove</button>
+      <button onClick={HandleSort}>Sort</button>
     </>
   );
 }
