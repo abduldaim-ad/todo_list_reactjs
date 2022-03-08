1fd7be022c5e18ae6b2d0cc5021d9c6659d56e67
ad551 <gdaim551@gmail.com>
2022-03-08 22:15:38 +0500

    CSS, Sorting List, Clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { useState } from "react";
+import './App.css';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button from '@mui/material/Button';
+import AnalogClock from 'analog-clock-react';
+import Checkbox from '@mui/material/Checkbox';
 
 const App = () =>{
   const [item, setItem] = useState("");
   const [itemList, setItemList] = useState([])
 
+  let options = {
+    width: "300px",
+    border: true,
+    borderColor: "green",
+    baseColor: "grey",
+    centerColor: "#2e2e2e",
+    centerBorderColor: "#ffffff",
+    handColors: {
+      second: "red",
+      minute: "#ffffff",
+      hour: "#ffffff"
+    }
+};
+
   const HandleAdd = () =>{
     if(item!==""){
-    const newList = [...itemList, item];
+    const newList = [...itemList, item].sort();
     setItemList(newList)
     return (
       <>
@@ -32,33 +52,29 @@ const App = () =>{
     }
   }
 
-  const HandleSort = () =>{
-    console.log(itemList.sort());
-    alert(itemList.sort());
-    //const sortedList = itemList.sort();
-    // if(item!==""){
-    //   setItemList(sortedList);
-    // }
-    // return (
-    //   <>
-    //   {sortedList}
-    //   {setItem("")}
-    //   </>
-    // );
-  }
-
   return(
     <>
-      <h1>ToDo List</h1>
-      <input value={item} onChange={(e)=>setItem(e.target.value)} placeholder="Add an item"/>
-      <ul>
-      {itemList.map((item) => {
-      return <li id={item} key={item}>{item}</li>;
-      })}
-      </ul>
-      <button onClick={HandleAdd}>Add</button>
-      <button onClick={HandleRemove}>Remove</button>
-      <button onClick={HandleSort}>Sort</button>
+      <h1 className="main-heading">ToDo List</h1>
+      <div className="main-div">
+        <div className="left-div">
+          <input value={item} onChange={(e)=>setItem(e.target.value)} placeholder="Add an item"/>
+            <ul className="listItems">
+            {itemList.map((item) => {
+            return <div className="alignment"><Checkbox className="check-box"/> <li id={item} key={item}>{item}</li></div>;
+            })}
+            </ul>
+
+          <Button className="add-btn" variant="contained" color="success" onClick={HandleAdd}>
+            <AddIcon/>
+          </Button>
+          <Button variant="outlined" color="error" onClick={HandleRemove}>
+            <DeleteIcon/>
+          </Button>
+        </div>
+        <div className="right-div">
+          <AnalogClock {...options} />
+        </div>
+      </div>
     </>
   );
 }
